1dcf5196530163494895677c09c304b891c5267e
jonas181199 <jonasschirm@web.de>
2021-05-02 01:15:24 +0200

    ...

--- src/Appenmeier/Kapitel74/Controller.java
+++ src/Appenmeier/Kapitel74/Controller.java
@@ -16,22 +16,14 @@ import java.util.Random;
         @FXML
         private ImageView diceImageView;
 
-        @FXML
-        Image image1 = new Image("@../../../../../../../Downloads/768px-NYCS-bull-trans-2.svg.png");
-
-        @FXML
-        Image image2 = new Image("@../../../../../../../Downloads/1024px-NYCS-bull-trans-1.svg.png");
-
-
-
         public void rollTheDice(ActionEvent event) {
             Random random = new Random();
             Integer randomNumber = random.nextInt(6) + 1;
             diceValueLabel.setText(randomNumber.toString());
 
-            Image image = new Image(getClass().getResourceAsStream("files/"+ random +".png"));
-            diceImageView.setFitHeight(200); //726
-            diceImageView.setFitWidth(200); //500
+            Image image = new Image(getClass().getResourceAsStream("files/"+ randomNumber +".png"));
+            diceImageView.setFitHeight(100); //726
+            diceImageView.setFitWidth(100); //500
             diceImageView.setImage(image);
 
 
